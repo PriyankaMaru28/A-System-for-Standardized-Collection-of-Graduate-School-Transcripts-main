ca90fb1c54488c4f7556877758b84c32c5e5d116
“PriyankaMaru28” <marupriyanka288@gmail.com>
2022-12-20 18:20:40 -0330

    separated the routes

--- backend/app.js
+++ backend/app.js
@@ -29,17 +29,19 @@ if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'){
   // set static folder
   app.use(express.static(path.resolve(__dirname,'..','frontend','build')))
 
-  app.get('*',(req,res)=>
+  app.get('/*',(req,res)=>
    {
     res.sendFile(path.resolve(__dirname,'..','frontend','build','index.html'))
   })
 
+}else{
+
+  app.use('/', require('./routes/index'));
+  app.use('/', require('./routes/users'));
+  app.use('/',require('./routes/student'));
 }
+  
 
-
-app.use('/', require('./routes/index'));
-app.use('/', require('./routes/users'));
-app.use('/',require('./routes/student'));
 
 
 
