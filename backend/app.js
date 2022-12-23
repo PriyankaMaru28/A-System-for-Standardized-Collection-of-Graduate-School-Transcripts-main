df68d8b6443c32be760dc787cd5057beecaf2b37
“PriyankaMaru28” <marupriyanka288@gmail.com>
2022-12-23 17:09:18 -0330

    backend changes

--- backend/app.js
+++ backend/app.js
@@ -12,14 +12,14 @@ var cors = require('cors');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname,'..','frontend','public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
@@ -38,9 +38,9 @@ if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'){
 
 }
 
-  app.use('/', require('./routes/index'));
-  app.use('/', require('./routes/users'));
-  app.use('/',require('./routes/student'));
+  app.use('/', indexRouter);
+  app.use('/', usersRouter);
+  app.use('/',studentRouter);
 
 
 // catch 404 and forward to error handler
