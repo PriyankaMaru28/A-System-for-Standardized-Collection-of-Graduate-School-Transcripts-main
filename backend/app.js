4fd0f36d5f1bf31d6dd074f153b92b84b8d0dbe7
“PriyankaMaru28” <marupriyanka288@gmail.com>
2022-12-23 16:07:54 -0330

    app.get() changes

--- backend/app.js
+++ backend/app.js
@@ -29,8 +29,10 @@ if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'){
   // set static folder
   app.use(express.static(path.resolve(__dirname,'..','frontend','build')))
 
-  app.get('*',(req,res)=>
+  app.get('/*',(req,res)=>
    {
+    console.log("GET REQUEST CALLLED...")
+    console.log('req... ',req)
     res.sendFile(path.resolve(__dirname,'..','frontend','build','index.html'))
   })
 
