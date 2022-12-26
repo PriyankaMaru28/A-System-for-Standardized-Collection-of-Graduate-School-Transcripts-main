63fbe9937380fb3256b55e0a32c5cf6ca61eadc0
“PriyankaMaru28” <marupriyanka288@gmail.com>
2022-12-26 15:54:53 -0330

    changed getStudentScore from get api to post api

--- frontend/src/Pages/GradOfficer/GradOfficerHome.js
+++ frontend/src/Pages/GradOfficer/GradOfficerHome.js
@@ -77,7 +77,7 @@ class GradOfficerHome extends React.Component {
 
   componentDidMount() {
     axios.defaults.headers.common["Authorization"] = JSON.parse(Cookies.get("session")).token;
-    axios.get('/getStudentScores').then((response) => {
+    axios.post('/getStudentScores').then((response) => {
       console.log("response...")
       console.log(response.data.scoreData)
       this.setState({
