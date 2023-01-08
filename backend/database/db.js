9bf4895a0ace60566e05e5c9b749caceedfd9830
Priyanka Maru <68162528+PriyankaMaru28@users.noreply.github.com>
2023-01-08 18:06:06 -0330

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,53 +1,7 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-// First you need to create a connection to the database
-// Be sure to replace 'user' and 'password' with the correct values
 
-// var db_config = {
-//   host: 'us-cdbr-east-06.cleardb.net',
-//   user: 'bfec5a8020c3c9',
-//   password: '07448e55',
-//   database: 'heroku_2e92b0443747f43',
-// }
-
-// var db_config = {
-//   host: '127.0.0.1',
-//   user: 'root',
-//   password: 'priyanka',
-//   database: 'project schema',
-// }
-
-// var con;
-
-// function handleDisconnect(){
-  
-//   con = mysql.createConnection(db_config)
-  
-
-// con.connect((err) => {
-//   if (err) {
-//     console.log(err)
-//     console.log('Error connecting to Db');
-//     setTimeout(handleDisconnect, 2000);
-//     return;
-//   }
-//   console.log('Database Connection established');
-// });
-
-// con.on('error', function(err) {
-//   console.log('db error', err);
-//   if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-//     handleDisconnect();                        
-//   } else {                                      
-//     throw err;                                  
-//   }
-// });
-
-// }
-
-// handleDisconnect()
-console.log("DATABASE>>>", process.env.REACT_APP_DB_HOST)
 /**
  * Pool connection created for the 
  * so the server is continuously connected. - Heroku
@@ -64,25 +18,6 @@ var pool = mysql.createPool({
   wait_timeout : 28800,
   connect_timeout :10
 });
-
-// Connection for database schema
-
-// const  con = mysql.createConnection({
-//   host: '127.0.0.1',
-//   user: 'root',
-//   password: 'priyanka',
-//   database: 'project schema',
-
-// })
-
-// con.connect((err) =>{
-//   if(err){
-//       console.log(err)
-//     console.log('Error connecting to Db');
-//     return;
-//   }
-//   console.log('Connection established');
-// });
 
 
 module.exports = pool;
